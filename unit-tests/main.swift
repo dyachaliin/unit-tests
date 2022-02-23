d9ecb0853fcb73fe90e5019e91101843e1fc6d3c
dyachaliin <dyachaliin@gmail.com>
2022-02-23 19:26:08 +0200

    Implemented Node class and List class, made addNode method

--- unit-tests/main.swift
+++ unit-tests/main.swift
@@ -8,4 +8,28 @@
 import Foundation
 
 
+class LinkedListNode {
+    var value: Character
+    var next: LinkedListNode?
+    init(value: Character) {
+        self.value = value
+        self.next = nil
+    }
+}
 
+class LinkedList {
+    var head: LinkedListNode?
+    var tail: LinkedListNode?
+    
+    func addNode(_ value: Character) {
+        let node: LinkedListNode? = LinkedListNode(value: value)
+        if (self.head == nil) {
+            self.head = node
+        } else {
+            self.tail!.next = node
+        }
+        node!.next = self.head
+        self.tail = node
+    }
+    
+}
