c10a35e608457efa70b9f4a840b4a22ad2a1f2d7
dyachaliin <dyachaliin@gmail.com>
2022-02-23 23:41:07 +0200

    Made insert and get methods

--- unit-tests/main.swift
+++ unit-tests/main.swift
@@ -11,9 +11,9 @@ import Foundation
 class LinkedListNode {
     var value: Character
     var next: LinkedListNode?
-    init(value: Character) {
+    init(value: Character, next: LinkedListNode? = nil) {
         self.value = value
-        self.next = nil
+        self.next = next
     }
 }
 
@@ -35,7 +35,7 @@ class LinkedList {
     }
     
     
-    func addNode(_ value: Character) {
+    func append(_ value: Character) {
         let node: LinkedListNode? = LinkedListNode(value: value)
         if (self.head == nil) {
             self.head = node
@@ -46,6 +46,46 @@ class LinkedList {
         self.tail = node
     }
     
+    
+    func getNode(at index: Int) -> LinkedListNode? {
+        if head == nil {
+            return nil
+        } else if index < 0 || index >= self.listLength() {
+            return nil
+        }
+        var currentNode = head
+        var currentIndex = 0
+        while currentNode != nil && currentIndex < index {
+            currentNode = currentNode?.next
+            currentIndex += 1
+        }
+        return currentNode
+    }
+    
+    
+    func insert(_ element: Character, at index: Int) {
+        if index < 0 || index >= self.listLength() {
+            self.append(element)
+            return
+        }
+        if let previousNode = self.getNode(at: index), let tail = tail {
+            if previousNode === tail {
+                self.append(element)
+            } else {
+                previousNode.next = LinkedListNode(value: element, next: previousNode.next)
+            }
+        } else {
+            self.append(element)
+        }
+    }
+    
 }
 
-
+//let list = LinkedList()
+//list.append("A")
+//list.append("B")
+//list.append("C")
+//list.append("D")
+//print(list.getNode(at: 10)?.value)
+//list.insert("E", at: 3)
+//print(list.getNode(at: 10)?.value)
